6b2137b962b569bdaa8978a9bdd19e4677337ccf
yosiquesealgo <yosiquesealgo>
2020-08-17 19:15:33 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import MaterialTable from "material-table";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar>
+        <Toolbar>
+          <Typography>Pagination API</Typography>
+        </Toolbar>
+      </AppBar>
+      <div style={{ height: "100px", width: "100%" }} />
+      <div>
+        <MaterialTable
+          title="Remote Data Preview"
+          columns={[
+            { title: "Id", field: "_id" },
+            { title: "Name", field: "name" },
+            { title: "Location", field: "location" },
+            {
+              title: "Created At",
+              field: "createdAt",
+              type: "datetime",
+            },
+            { title: "Updated At", field: "updatedAt", type: "datetime" },
+          ]}
+          data={(query) =>
+            new Promise(async (resolve, reject) => {
+              let url = "http://localhost/api/stores?";
+              url += "limit=" + query.pageSize;
+              url += "&page=" + parseInt(query.page + 1);
+
+              fetch(url)
+                .then((response) => response.json())
+                .then((result) => {
+                  resolve({
+                    data: result.docs,
+                    page: result.page - 1,
+                    totalCount: result.totalDocs,
+                  });
+                });
+            })
+          }
+        />
+      </div>
     </div>
   );
 }
